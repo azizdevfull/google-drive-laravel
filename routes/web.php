e7be7e04777a909d16fa7931f0885cdf71d1f05b
Azizbek Isroilov <aziz16110904@gmail.com>
2023-08-23 11:03:17 +0500

    completed file upload to drive

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GoogleDriveController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('google/login',[GoogleDriveController::class,'googleLogin'])->name('google.login');
+Route::get('google-drive/file-upload',[GoogleDriveController::class,'googleDriveFilePpload'])->name('google.drive.file.upload');
